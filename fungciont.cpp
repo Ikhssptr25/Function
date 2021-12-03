73fd74a0c8631e6087746db40262806ce0b7abb9
Ikhsan saputra <92314859+Ikhssptr25@users.noreply.github.com>
2021-12-03 07:30:04 +0700

    Add files via upload

---
+++ fungciont.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+float perpangkatan(float a,float b);
+int main(){
+	cout<<perpangkatan(2,3);
+	
+}
+float perpangkatan (float a,float b){
+	float hasil=1;
+	for(int i=0; i<b; i++){
+		hasil=hasil*a;}
+		return hasil;
+	 
+}
